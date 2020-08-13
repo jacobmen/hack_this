495a0c93ff4231b6846a521ddd35fcdb56a1f8cf
Rohan Vasavada <rohanvasavada@gmail.com>
2020-08-13 12:51:51 -0500

    added sign in functionality

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,63 @@
-import { createMuiTheme, responsiveFontSizes, CssBaseline, ThemeProvider, AppBar, Toolbar } from '@material-ui/core'
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import {
+    createMuiTheme,
+    Button,
+    responsiveFontSizes,
+    CssBaseline,
+    ThemeProvider,
+    AppBar,
+    Toolbar
+} from '@material-ui/core'
+import {firebase, auth} from '../services/firebase'
 import Head from 'next/head'
 
 export default function Layout(props) {
+    const [signedIn, setSignedIn] = React.useState(false)
+    const router = useRouter()
+
     const theme = responsiveFontSizes(createMuiTheme({
         palette: {
             type: 'dark',
         },
     }));
 
+    React.useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setSignedIn(true)
+            } else {
+                setSignedIn(false)
+            }
+        })
+    })
+
+    const handleClick = () => {
+        if (signedIn) {
+            auth.signOut()
+        }
+
+        router.push('./login')
+    }
+
     return (
         <>
             <Head>
                 <title>Historio</title>
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet"
+                      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
             </Head>
             <ThemeProvider theme={theme}>
                 <CssBaseline>
-                    <AppBar position="static"><Toolbar></Toolbar></AppBar>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Button onClick={handleClick}>{signedIn ? "Sign Out" : "Sign In"}</Button>
+                        </Toolbar>
+                    </AppBar>
                     {props.children}
                 </CssBaseline>
             </ThemeProvider>
--- pages/createaccount.js
+++ pages/createaccount.js
@@ -1,48 +1,60 @@
 import React from 'react';
-import {useRouter} from "next/router";
-import Link from 'next/link'
+import {useRouter} from 'next/router'
 import {firebase, auth} from '../services/firebase'
 import {Paper, Typography, FormControl, InputLabel, Input, Grid, Button} from '@material-ui/core'
 import Layout from '../components/layout'
 import styles from '../styles/main.module.scss'
 
-export default function login(props) {
+export default function createAccount(props) {
     const [email, setEmail] = React.useState("")
     const [password, setPassword] = React.useState("")
+    const [confirmPassword, setConfirmPassword] = React.useState("")
 
     const [errorMessage, setErrorMessage] = React.useState("")
 
     const router = useRouter()
 
-    const handleSignIn = (e) => {
+    const handleCreateAccount = (e) => {
         e.preventDefault()
 
-        auth.signInWithEmailAndPassword(email, password)
-            .then((user) => {
-                router.push('./')
-            })
-            .catch((error) => {
-                setErrorMessage(error.message)
-            })
+        if (password.trim() === confirmPassword.trim()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .then((user) => {
+                    router.push('./')
+                })
+                .catch((error) => {
+                    setErrorMessage(error.message)
+                })
+        } else {
+            setErrorMessage("Passwords do not match")
+        }
     }
 
     return (
         <Layout>
             <div className={styles.loginContainer}>
-                <Typography variant="h4" className={styles.loginContainer__header}>Sign In</Typography>
+                <div className={styles.loginContainer__header}>
+                    <Typography variant="h4">Create Account</Typography>
+                    <Typography variant="subtitle1">Must have a .edu email</Typography>
+                </div>
                 <Paper elevation={1} className={styles.loginContainer__paper}>
-                    <form onSubmit={handleSignIn}>
+                    <form onSubmit={handleCreateAccount}>
                         <Grid container direction={"column"}>
                             <FormControl margin={"normal"} required>
                                 <InputLabel htmlFor="email">Email address</InputLabel>
-                                <Input id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <Input id="email" value={email} inputProps={{pattern: ".*edu$"}}
+                                       onChange={(e) => setEmail(e.target.value)}/>
                             </FormControl>
                             <FormControl margin={"normal"} required>
                                 <InputLabel htmlFor="password">Password</InputLabel>
                                 <Input id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                             </FormControl>
+                            <FormControl margin={"normal"} required>
+                                <InputLabel htmlFor="password">Confirm Password</InputLabel>
+                                <Input id="confirmPassword" value={confirmPassword}
+                                       onChange={(e) => setConfirmPassword(e.target.value)}/>
+                            </FormControl>
                             <Button type="submit">Submit</Button>
-                            <Link href="./createaccount"><a>Create Account</a></Link>
                             <Typography variant="body1">{errorMessage}</Typography>
                         </Grid>
                     </form>
